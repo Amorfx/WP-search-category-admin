3e0894ba095336bbaa0c5eecbb3e95382e4fa834
Clément DECOU <Clment DECOU>
2017-03-19 18:49:22 +0100

    fix: use plugin dir url

--- wp-search-cat-admin.php
+++ wp-search-cat-admin.php
@@ -15,8 +15,8 @@ function register_wpsca_custom_script($hook)
         return;
     }
 
-    wp_register_script('wpsca_admin', site_url().'/wp-content/plugins/wp-search-cat-admin/js/debounce.min.js');
-    wp_register_script('wpsca_debounce', site_url().'/wp-content/plugins/wp-search-cat-admin/js/script.js');
+    wp_register_script('wpsca_admin', plugin_dir_url( __FILE__ ).'js/debounce.min.js');
+    wp_register_script('wpsca_debounce', plugin_dir_url( __FILE__ ).'js/script.js');
     wp_enqueue_script('wpsca_debounce', false, array(), false, true);
     wp_enqueue_script('wpsca_admin', false, array(), false, true);
 }
